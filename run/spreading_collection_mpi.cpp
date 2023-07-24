8614e8ae1b82d34e5947146c7fa3ae539672ef96
Giorgio Audrito <giorgio.audrito@gmail.com>
2023-07-24 19:15:59 +0200

    polishing spreading_collection_mpi.cpp

--- run/spreading_collection_mpi.cpp
+++ run/spreading_collection_mpi.cpp
@@ -6,36 +6,14 @@
  */
 
 #include "lib/spreading_collection.hpp"
-#include <mpi.h>
 
 using namespace fcpp;
 
 
-void validate_result(option::plot_t& p) {
-    int n_procs;
-    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
-    option::plot_t q;
-        //! @brief The component type (batch simulator with given options).
-		using comp_t = component::batch_simulator<option::list>;
-        //! @brief The list of initialisation values to be used for simulations.
-
-        auto init_list = batch::make_tagged_tuple_sequence(
-            batch::arithmetic<option::seed>(0, 9, 1),                   // 10 different random seeds
-            batch::arithmetic<option::speed>(size_t(0), comm/2, comm/20), // 11 different speeds
-            // generate output file name for the run
-            batch::stringify<option::output>("output/spreading_collection_batch", "txt"),
-            batch::constant<option::plotter>(&q)                        // reference to the plotter object
-        );
-        //! @brief Runs the given simulations.
-        batch::run(comp_t{}, common::tags::dynamic_execution{}, init_list);
-    
-    if (p == q)
-        std::cerr << "Test succeed!" << std::endl;
-    else
-        std::cerr << "Test failed!" << std::endl; 
-}
-
 int main(int argc, char** argv) {
+    std::cerr << "ARGS: " << argc;
+    for (int i=0; i<argc; ++i) std::cerr << " " << argv[i];
+    std::cerr << std::endl;
     MPI_Init(&argc, &argv);
     //! @brief Construct the plotter object.
     option::plot_t p;
@@ -68,8 +46,11 @@ int main(int argc, char** argv) {
     batch::mpi_run(comp_t{}, common::tags::dynamic_execution{}, init_list);
     //! @brief Builds the resulting plots.
     if (rank == 0) {
-        validate_result(p);
-        std::cout << plot::file("distributed_batch", p.build());
+        option::plot_t q;
+        swap(p, q);
+        batch::run(comp_t{}, common::tags::dynamic_execution{}, init_list);
+        std::cerr << (p == q ? "Test succeeded!" : "Test failed!") << std::endl;
+        std::cout << plot::file("distributed_batch", q.build());
     }
     MPI_Finalize();
     return 0;
