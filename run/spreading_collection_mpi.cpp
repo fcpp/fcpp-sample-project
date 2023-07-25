d0078f1141edfc2bb432f4e53846a4cc81396555
Giorgio Audrito <giorgio.audrito@gmail.com>
2023-07-25 18:40:22 +0200

    conveniently bundling all mpi runs into a single execution

--- run/spreading_collection_mpi.cpp
+++ run/spreading_collection_mpi.cpp
@@ -29,15 +29,9 @@ class profiler {
     std::clock_t start;
 };
 
-
-int main(int argc, char** argv) {
-    // Construct the plotter object.
-    option::plot_t p;
-    // The component type (batch simulator with given options).
-    using comp_t = component::batch_simulator<option::list>;
-    // The list of initialisation values to be used for simulations.
-    auto init_list = batch::make_tagged_tuple_sequence(
-        batch::arithmetic<option::seed >(0, 99, 1),     // 100 different random seeds
+auto init_lister(option::plot_t& p, int max_seed) {
+    return batch::make_tagged_tuple_sequence(
+        batch::arithmetic<option::seed >(0, max_seed-1, 1), // max_seed different random seeds
         batch::arithmetic<option::speed>(0, 48, 2, 10), // 25 different speeds
         batch::arithmetic<option::dens >(5, 29, 1, 10), // 25 different densities
         batch::arithmetic<option::hops >(1, 25, 1, 10), // 25 different hop sizes
@@ -56,26 +50,121 @@ int main(int argc, char** argv) {
         }),
         batch::constant<option::plotter>(&p) // reference to the plotter object
     );
-    // Runs the given simulations.
-    profiler t;
-    batch::mpi_run(comp_t{}, common::tags::dynamic_execution{}, init_list);
-    std::cerr << double(t) << std::endl;
-    // Builds the resulting plots.
-    std::cout << plot::file("distributed_batch", p.build());
-    // Checks correctness.
-    if (p != option::plot_t{}) {
-        option::plot_t q;
-        swap(p, q);
+}
+
+#ifndef FCPP_MPI
+#define MPI_Init_thread(...)
+#define MPI_Comm_rank(...)
+#define MPI_Comm_size(...)
+#define MPI_Barrier(...)
+#define MPI_Finalize()
+#endif
+
+int main(int argc, char** argv) {
+    // Sets up MPI.
+    int provided, rank, n_procs;
+    MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
+    //assert(provided == MPI_THREAD_MULTIPLE);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
+    // The component type (batch simulator with given options).
+    using comp_t = component::batch_simulator<option::list>;
+    // Compute a reference plot.
+    option::plot_t q;
+    if (rank == 0) {
+        q = {};
+        auto init_list = init_lister(q, 100);
         batch::run(comp_t{}, common::tags::dynamic_execution{}, init_list);
-        std::stringstream sp, sq;
-        sp << std::setprecision(3) << plot::file("distributed_batch", p.build());
-        sq << std::setprecision(3) << plot::file("distributed_batch", q.build());
-        if (sp.str() == sq.str()) {
-            std::cerr << "Test succeeded!" << std::endl;
-        } else {
-            std::cerr << "Test failed!" << std::endl;
-            std::cout << sp.str();
+    }
+    // The vector storing recorded execution times.
+    std::vector<double> t_static, t_dynamic;
+    // Construct the plotter object.
+    option::plot_t p;
+    for (int i=0; i<10; ++i) {
+        p = {};
+        MPI_Barrier(MPI_COMM_WORLD);
+        profiler t;
+        // The list of initialisation values to be used for simulations.
+        auto init_list = init_lister(p, 100);
+        // Runs the given simulations.
+        batch::mpi_run(comp_t{}, common::tags::dynamic_execution{}, init_list);
+        if (rank == 0) {
+            t_static.push_back(t);
+            std::stringstream sp, sq;
+            sp << std::setprecision(3) << plot::file("distributed_batch", p.build());
+            sq << std::setprecision(3) << plot::file("distributed_batch", q.build());
+            if (sp.str() != sq.str()) {
+                std::cerr << "MPI static run " << i << ", check failed!" << std::endl;
+                std::cout << sp.str();
+                std::cout << sq.str();
+            }
         }
     }
+    for (int i=0; i<10; ++i) {
+        p = {};
+        MPI_Barrier(MPI_COMM_WORLD);
+        profiler t;
+        // The list of initialisation values to be used for simulations.
+        auto init_list = init_lister(p, 100);
+        // Runs the given simulations.
+        batch::mpi_dynamic_run(comp_t{}, common::tags::dynamic_execution{}, init_list);
+        if (rank == 0) {
+            t_dynamic.push_back(t);
+            std::stringstream sp, sq;
+            sp << std::setprecision(3) << plot::file("distributed_batch", p.build());
+            sq << std::setprecision(3) << plot::file("distributed_batch", q.build());
+            if (sp.str() != sq.str()) {
+                std::cerr << "MPI dynamic run " << i << ", check failed!" << std::endl;
+                std::cout << sp.str();
+                std::cout << sq.str();
+            }
+        }
+    }
+    if (rank == 0) {
+        q = {};
+        auto init_list = init_lister(q, 10*n_procs);
+        batch::run(comp_t{}, common::tags::dynamic_execution{}, init_list);
+    }
+    for (int i=0; i<10; ++i) {
+        p = {};
+        MPI_Barrier(MPI_COMM_WORLD);
+        profiler t;
+        // The list of initialisation values to be used for simulations.
+        auto init_list = init_lister(p, 10*n_procs);
+        // Runs the given simulations.
+        batch::mpi_run(comp_t{}, common::tags::dynamic_execution{}, init_list);
+        if (rank == 0) {
+            t_static.push_back(t);
+            std::stringstream sp, sq;
+            sp << std::setprecision(3) << plot::file("distributed_batch", p.build());
+            sq << std::setprecision(3) << plot::file("distributed_batch", q.build());
+            if (sp.str() != sq.str()) {
+                std::cerr << "MPI static run " << i << ", check failed!" << std::endl;
+                std::cout << sp.str();
+                std::cout << sq.str();
+            }
+        }
+    }
+    for (int i=0; i<10; ++i) {
+        p = {};
+        MPI_Barrier(MPI_COMM_WORLD);
+        profiler t;
+        // The list of initialisation values to be used for simulations.
+        auto init_list = init_lister(p, 10*n_procs);
+        // Runs the given simulations.
+        batch::mpi_dynamic_run(comp_t{}, common::tags::dynamic_execution{}, init_list);
+        if (rank == 0) {
+            t_dynamic.push_back(t);
+            std::stringstream sp, sq;
+            sp << std::setprecision(3) << plot::file("distributed_batch", p.build());
+            sq << std::setprecision(3) << plot::file("distributed_batch", q.build());
+            if (sp.str() != sq.str()) {
+                std::cerr << "MPI dynamic run " << i << ", check failed!" << std::endl;
+                std::cout << sp.str();
+                std::cout << sq.str();
+            }
+        }
+    }
+    MPI_Finalize();
     return 0;
 }
